f1bbad4471abd72a03596565d0c75a0024d64338
EduardoNagano <eduardo_nagano@yahoo.com.br>
2021-06-08 10:57:20 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,9 +1,9 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -19,6 +19,11 @@ namespace SalesWebMvc.Services
         public List<Department> FindAll()
         {
             return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+        public async Task<List<Department>> FindAllAsync()
+        {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
